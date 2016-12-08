dbdb9615e4b93d9f582a4afc28ad80dbf2ea2dab
kevinolea123 <dankevinolea@gmail.com>
2016-12-08 14:09:47 +0800

    Update getProv.php

--- hanapbuhay/getProv.php
+++ hanapbuhay/getProv.php
@@ -1,20 +1,16 @@
 <?php
 require "../zxcd9.php";
 if(!empty($_POST["region"])) {
-
 		$reg = test_input($_POST["region"]);
-
 			  $query = "SELECT * FROM lib_provinces WHERE regid = :reg"; 
               $query_params = array(':reg' => $reg);
               try 
               { $stmt = $db->prepare($query); $result = $stmt->execute($query_params); } 
               catch(PDOException $ex) 
               { die("Failed to run query: " . $ex->getMessage()); } 
-              
               while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                  echo "<option value='".$row["provid"]."'>".$row['provname']."</option>";
           	  }
-
 } else {
 	echo "boom";
 }
