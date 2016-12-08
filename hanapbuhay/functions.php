bf988a17fe5e9f7d5fddb65b1965f03b4a724f5e
kevinolea123 <dankevinolea@gmail.com>
2016-12-08 14:12:43 +0800

    Update functions.php

--- hanapbuhay/functions.php
+++ hanapbuhay/functions.php
@@ -1,18 +1,15 @@
 <?php
 require "../zxcd9.php";
-
 //start post
 if(!empty($_POST)) 
 { 
 //filter input
 //$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
     if($_POST['action'] == "editparticipant") {
         $id = test_input($_POST['pid']);
         $remarks = test_input($_POST['remarks']);
         $parts = explode('/', $_POST['birthdate']);
         $bdate  = "$parts[2]-$parts[0]-$parts[1]";
-
         $stmt = $db->prepare("UPDATE PRTsupply SET
             firstname=:firstname, middlename=:middlename, lastname=:lastname, sex=:sex, birthdate=:birthdate, education=:hea, pantawidid=:pantawidid, contactnumber=:contact, height=:height, remarks=:remarks, hasNSO=:hasnso, hasNBI=:hasnbi WHERE id=:id");
         $stmt->bindParam(':id', $id);
@@ -29,25 +26,17 @@ if(!empty($_POST))
         $stmt->bindParam(':hasnso', $_POST['hasnso']);
         $stmt->bindParam(':hasnbi', $_POST['hasnbi']);
         $stmt->execute();
-
         echo "edited";
     }
-
     if($_POST['action'] == "deleteparticipant") {
         $id = test_input($_POST['pid']);
-
         $stmt = $db->prepare("DELETE FROM PRTsupply WHERE id=:id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-
          $stmt = $db->prepare("DELETE FROM PRTsupplytags WHERE supplyrefid=:id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-
         echo "deleted";
     }
-
-
 }//end post
-     
 ?>
