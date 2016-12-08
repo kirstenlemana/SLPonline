b06582fc74dcd27049064c99d6bb0ba0af7db7c0
jcaceli <jcaceli@e-dswd.net>
2016-12-08 11:09:10 +0800

    udpate main.php

--- main.php
+++ main.php
@@ -377,44 +377,15 @@ h3 {
       </div>
     </div>
     <div class="row" style="margin-top:1em">
-      <div style="border:solid 1px #c5d6de;margin-left:1em;background:#fff;text-align:center;padding:0;">
-
-        <!--shoutbox table-->
-        <table class="table table-bordered" style="margin-top:0em;line-height:0.9;vertical-align:middle;border:0;" id="shoutbox">
-          <thead style="background:#f6f8fa">
-            <th><b>National Shoutbox</b></th>
-          </thead>
-          <tbody style="height:300px;overflow-y:scroll;display:block">
-<?php
-      $stmtcom = $db->prepare("SELECT t.firstname, m.msg, m.added, t.region, t.id FROM shoutbox m LEFT JOIN hr_db t ON m.hrdbid=t.id ORDER BY m.id DESC LIMIT 15");
-      $stmtcom->execute();
-      while ($row8 = $stmtcom->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-            echo '<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad" style="vertical-align:middle" data-bg-text="'.timeago(strtotime($row8[2])).'"><div class="col-sm-12 nopad" style="text-align:left;line-height:1.1;padding-left:0.7em">'.$row8[1].' -<a href="hr/user.php?id='.$row8[4].'" style="color:#00ADDe;text-decoration:none">'.ucwords(strtolower($row8[0])).'</a></span><span style="color:#888;"> ('.$row8[3].')</span></div><div class="clearfix"></div></div></td></tr>';
-      }
-?>
-          </tbody>
-        </table>
-        <!--shoutbox table-->
-
-
-        <div class="form-group" style="margin:0;padding:0 1em 0 1em;margin-bottom:1em">
-                    <div class="input-group" style="margin-bottom:0;margin-top:1em">
-                      <input id="comment" name="comment" class="form-control" placeholder="" style="height:31.4px">
-                      <div class="input-group-btn">
-                        <button id="postcomment" class="btn btn-primary" style="padding:2px 8px 4px 8px">Post</button>
-                      </div>
-                    </div><!-- /input-group -->
-        </div>
-        <div class="clearfix"></div>
-      </div>
-    </div>
-  </div>
-    <div class="col-md-3" style="padding:1em;padding-top:0;padding-right:0">
-
-  <div class="row" style="padding-left:1em;padding-right:0.5em"> 
-    <div style="border:solid 1px #c5d6de;margin-left:1em;background:#fff;padding:1em;padding-left:1.5em;padding-right:1.5em;font-size:12px;padding-bottom:0">
+  <div style="background:#f6f8fa;font-size:15px;padding:1em;padding-left:1.5em;border:solid 1px #c5d6de;margin-left:0.9em;text-align:center;padding-top:0.8em;padding-bottom:0.8em">
+            <b>Recent Activities</b>
+          </div>
+       
+    <div style="border:solid 1px #c5d6de;margin-left:1.1em;background:#fff;padding:1em;padding-left:1.5em;padding-right:1.5em;font-size:12px;padding-bottom:0">
+       
+          
      <?php 
-    $stmt=$db->prepare("SELECT w.wall_msg,h.firstname,h.sex,w.wallposted,p.name,h.lastname,h.id,w.wallowner,h1.firstname,h1.lastname,w.wallposter FROM hr_wallposts as w LEFT JOIN hr_db as h on w.wallposter=h.id LEFT JOIN hr_profilepics as p on p.hrdbid=h.id LEFT JOIN hr_db as h1 on h1.id=w.wallowner ORDER BY w.wallpostid DESC LIMIT 0,10");
+    $stmt=$db->prepare("SELECT w.wall_msg,h.firstname,h.sex,w.wallposted,p.name,h.lastname,h.id,w.wallowner,h1.firstname,h1.lastname,w.wallposter FROM hr_wallposts as w LEFT JOIN hr_db as h on w.wallposter=h.id LEFT JOIN hr_profilepics as p on p.hrdbid=h.id LEFT JOIN hr_db as h1 on h1.id=w.wallowner ORDER BY w.wallpostid DESC LIMIT 0,5");
     $stmt->execute();
     while($rowWP=$stmt->fetch(PDO::FETCH_NUM,pdo::FETCH_ORI_NEXT)) { 
 
@@ -453,9 +424,61 @@ h3 {
     ?>
 
     </div>
+   </div>
+  </div>
+    <div class="col-md-3" style="padding:1em;padding-top:0;padding-right:0">
+
+  <div class="row" style="padding-left:1em;padding-right:0.5em"> 
+<!--<div style="border:solid 1px #c5d6de;margin-left:1em;background:#fff;padding:1em;padding-left:1.5em;padding-right:1.5em;font-size:12px;padding-bottom:0"> -->
+  <div style="border:solid 1px #c5d6de;margin-left:1em;background:#fff;padding:0;">
+
+        <!--shoutbox table-->
+        <table class="table table-bordered" style="margin-top:0em;line-height:0.9;vertical-align:middle;border:0;padding:0" id="shoutbox">
+          <thead style="background:#f6f8fa;">
+            <th style="font-size:15px"><b>National Shoutbox</b></th>
+          </thead> 
+        
+       <!--   <tbody style="height:800px;overflow-y:scroll;display:block"> -->
+            <tbody style="height:800px;overflow:hidden;display:block">
+<?php
+      $stmtcom = $db->prepare("SELECT t.firstname, m.msg, m.added, t.region, t.id,hp.name,t.lastname,t.sex,hp.name FROM shoutbox as m LEFT JOIN hr_db as t ON m.hrdbid=t.id LEFT JOIN hr_profilepics as hp on hp.hrdbid=t.id  ORDER BY m.id DESC LIMIT 10");
+      $stmtcom->execute();
+      while ($row8 = $stmtcom->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+         //   echo '<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad" style="vertical-align:middle" data-bg-text="'.timeago(strtotime($row8[2])).'"><div class="col-sm-12 nopad" style="text-align:left;line-height:1.1;padding-left:0.7em">'.$row8[1].' -<a href="hr/user.php?id='.$row8[4].'" style="color:#00ADDe;text-decoration:none">'.ucwords(strtolower($row8[0])).'</a></span><span style="color:#888;"> ('.$row8[3].')</span></div><div class="clearfix"></div></div></td></tr>';
+        if($row8[8]=="") {
+              if($row8[7]==0) {
+                  echo '<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad"><div class="col-md-3"><a style="text-decoration:none;" href="../hr/user.php?id='.$row8[4].'"><img src="../imgs/partner.png" width="50" height="50" style="border-radius:50%"></div><div class="col-md-9"><b>'.$row8[0].' '.$row8[6].'</a> ('.$row8[3].')<br><span style="color:#999;font-size:9px">'.date("M j - h:i a",strtotime($row8[2])).'</span><br><span style="color:#2c3e50;font-size:12px;font-weight: normal;">'.$row8[1].'</span></div></div></td></tr>';
+              } else {
+                  echo '<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad"><div class="col-md-3"><a style="text-decoration:none;" href="../hr/user.php?id='.$row8[4].'"><img src="../imgs/female.png" width="50" height="50" style="border-radius:50%"></div><div class="col-md-9"><b>'.$row8[0].' '.$row8[6].'</a> ('.$row8[3].')<br><span style="color:#999;font-size:9px">'.date("M j - h:i a",strtotime($row8[2])).'</span><br><span style="color:#2c3e50;font-size:12px;font-weight: normal;">'.$row8[1].'</span></div></div></td></tr>';
+              }
+        } else {
+          
+              echo '<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad"><div class="col-md-3"><a style="text-decoration:none;" href="../hr/user.php?id='.$row8[4].'"><img src="../docs/profilepics/'.$row8[5].'" width="50" height="50" style="border-radius:50%"></div><div class="col-md-9"><b>'.$row8[0].' '.$row8[6].'</a> ('.$row8[3].')<br><span style="color:#999;font-size:9px">'.date("M j - h:i a",strtotime($row8[2])).'</span><br><span style="color:#2c3e50;font-size:12px;font-weight: normal;">'.$row8[1].'</span></div></div></td></tr>';
+        }        
+
+
+
+      }
+?>
+          </tbody>
+        </table>
+        <!--shoutbox table-->
+
+
+        <div class="form-group" style="margin:0;padding:0 1em 0 1em;margin-bottom:1em;">
+                    <div class="input-group" style="margin-bottom:0;margin-top:5em">
+                      <input id="comment" name="comment" class="form-control" placeholder="" style="height:31.4px">
+                      <div class="input-group-btn">
+                        <button id="postcomment" class="btn btn-primary" style="padding:2px 8px 4px 8px">Post</button>
+                      </div>
+                    </div><!-- /input-group -->
+        </div>
+        <div class="clearfix"></div>
+      </div>
+
   </div>
   </div>
-  <div class="col-md-3" style="padding:1em;padding-top:0;padding-right:0">
+  <div class="col-md-3" style="padding:1em;padding-top:0;padding-right:0;padding-left:0.5em">
 
 
 <div class="row" style="padding-top:0em;padding-left:1em;padding-right:0.5em">
@@ -665,7 +688,12 @@ if ($("#comment").val() == "") {
                    success: function(data)
                    {
                       if (data == "commented") {
-                        $("#shoutbox").prepend('<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad" style="vertical-align:middle" data-bg-text="now"><div class="col-sm-12 nopad" style="text-align:left;line-height:1.1;padding-left:1em"> '+$('input[name=comment]').val()+' -<a href="http://slp.ph/hr/viewdata/user.php?id=" style="color:#00ADDe;text-decoration:none"><?php echo $_SESSION["firstname"]; ?></a></span><span style="color:#888;"> (<?php echo $_SESSION["filter"];?>)</span></div><div class="clearfix"></div></div></td></tr>');
+                        $("#shoutbox").prepend('<tr><td style="font-size:12px;vertical-align:middle"><div class="row nopad"><div class="col-md-3"><a style="text-decoration:none;" href="../hr/user.php?id="><img src="../imgs/partner.png" width="50" height="50" style="border-radius:50%"></div><div class="col-md-9"><a href="http://slp.ph/hr/viewdata/user.php?id=" style="font-weight:bold;text-decoration:none"><?php echo $_SESSION["firstname"].' '.$_SESSION["lastname"]; ?></a>&nbsp;</span><span style="font-weight:bold">(<?php echo $_SESSION["filter"];?>)</span> <span style="color:#888;"> <br>now</span><br>'+$('input[name=comment]').val()+'</div><div class="clearfix"></div></div></td></tr>');
+                        
+                          
+
+
+
                         $("#comment").val("");
                       } else {
                         alert(data);
