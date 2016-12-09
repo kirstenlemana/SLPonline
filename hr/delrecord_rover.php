ae76b499ae8dfe35db32bb0b43bb2c41e2b00cc0
kirstenlemana <kirstenlemana@gmail.com>
2016-12-09 10:02:46 +0800

    Update delrecord_rover.php - Adjust spacing and curly braces

--- hr/delrecord_rover.php
+++ hr/delrecord_rover.php
@@ -1,18 +1,13 @@
 <?php
 require "../zxcd9.php";
 //start post
-if(!empty($_POST)) 
-{ 
+if(!empty($_POST)) { 
 //filter input
 $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-
         $id = test_input($_POST['id']);
-
         $query = " DELETE FROM hr_rover WHERE id = :id";
         $query_params = array( 
-            ':id' => $id
-        ); 
+            ':id' => $id); 
         try { 
             $stmt = $db->prepare($query); 
             $result = $stmt->execute($query_params); 
@@ -20,7 +15,6 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         catch(PDOException $ex) { 
             die("Failed to run query: " . $ex->getMessage()); 
         } 
-
         $query = " DELETE FROM hr_RVtags WHERE roverid = :id";
         $query_params = array( 
             ':id' => $id
@@ -32,8 +26,6 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         catch(PDOException $ex) { 
             die("Failed to run query: " . $ex->getMessage()); 
         } 
-
         echo "good";
 }//end post
-     
 ?>
