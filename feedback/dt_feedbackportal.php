4f9f286d1a3b6e6c43dbe85d88883b8f1751ba64
kirstenlemana <kirstenlemana@gmail.com>
2016-12-09 11:13:50 +0800

    Update dt_feedbackportal.php - Adjust spacing and curly braces

--- feedback/dt_feedbackportal.php
+++ feedback/dt_feedbackportal.php
@@ -5,10 +5,8 @@
  * Copyright: 2010 - Allan Jardine
  * License:   GPL v2 or BSD (3-point)
  */
-  
 class TableData {
- 
- 	private $_db;
+  	private $_db;
 	public function __construct() {
 		require("../l33tz9.php");
 	}
@@ -18,7 +16,6 @@ class TableData {
 		if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' ) {
 			$sLimit = "LIMIT ".intval( $_GET['iDisplayStart'] ).", ".intval( $_GET['iDisplayLength'] );
 		}
-		
 		// Ordering
 		$sOrder = "";
 		if ( isset( $_GET['iSortCol_0'] ) ) {
@@ -29,13 +26,11 @@ class TableData {
 					$sOrder .= "`".$columns[ intval( $_GET['iSortCol_'.$i] ) ]."` ". $sortDir .", ";
 				}
 			}
-			
 			$sOrder = substr_replace( $sOrder, "", -2 );
 			if ( $sOrder == "ORDER BY" ) {
 				$sOrder = "";
 			}
 		}
-		
 		/* 
 		 * Filtering
 		 * NOTE this does not match the built-in DataTables filtering which does it
@@ -53,7 +48,6 @@ class TableData {
 			$sWhere = substr_replace( $sWhere, "", -3 );
 			$sWhere .= ')';
 		}
-		
 		// Individual column filtering
 		for ( $i=0 ; $i<count($columns) ; $i++ ) {
 			if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' ) {
@@ -66,7 +60,6 @@ class TableData {
 				$sWhere .= "`".$columns[$i]."` LIKE :search".$i." ";
 			}
 		}
-		
 		/* SQL queries get data to display
 		a - hrfeedbackquestion
 		b - hrdb
@@ -85,9 +78,7 @@ class TableData {
 			LEFT JOIN hrf_replies as c ON c.qid=a.id
 			LEFT JOIN hr_db as d ON d.id=a.latestfeedbacker
 			GROUP BY a.id";
-
 		$statement = $this->_db->prepare($sQuery);
-		
 		// Bind parameters
 		if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" ) {
 			$statement->bindValue(':search', '%'.$_GET['sSearch'].'%', PDO::PARAM_STR);
@@ -99,13 +90,10 @@ class TableData {
 		}
 		$statement->execute();
 		$rResult = $statement->fetchAll();
-		
 		$iFilteredTotal = current($this->_db->query('SELECT FOUND_ROWS()')->fetch());
-		
 		// Get total number of rows in table
 		$sQuery = "SELECT COUNT(`".$index_column."`) FROM `".$table."`";
 		$iTotal = current($this->_db->query($sQuery)->fetch());
-		
 		// Output
 		$output = array(
 			"sEcho" => intval($_GET['sEcho']),
@@ -113,7 +101,6 @@ class TableData {
 			"iTotalDisplayRecords" => $iFilteredTotal,
 			"aaData" => array()
 		);
-		
 		// Return array of values
 		foreach($rResult as $aRow) {
 			$row = array();			
@@ -128,14 +115,12 @@ class TableData {
 			}
 			$output['aaData'][] = $row;
 		}
-		
 		echo json_encode( $output );
 	}
 }
 header('Pragma: no-cache');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 // Create instance of TableData class
-
 $table_data = new TableData();
 // Get the data
 $table_data->get('hr_feedbackquestion', 'id', array('id','subject','first','mid','last','count','dt','lastfeedbacker'));
@@ -145,8 +130,7 @@ $table_data->get('hr_feedbackquestion', 'id', array('id','subject','first','mid'
  *
  * RewriteRule ^pagename/data/?$ data.php?_page=PAGENAME [L,NC,QSA]
  *
- 
-switch ($_SERVER['REQUEST_METHOD']) {
+ switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         if (isset($_REQUEST['_page'])) {
         	if($_REQUEST['_page'] === 'PAGENAME') {
