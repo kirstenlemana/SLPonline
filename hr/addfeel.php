054961f6f08e579666bf78ac2e28a402d19c94c3
kirstenlemana <kirstenlemana@gmail.com>
2016-12-09 09:48:12 +0800

    Update addfeel.php - Adjust spacing and curly braces

--- hr/addfeel.php
+++ hr/addfeel.php
@@ -1,26 +1,19 @@
 <?php
 require "../zxcd9.php";
 //start post
-if(!empty($_POST)) 
-{ 
+if(!empty($_POST)) { 
 //filter input
 $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
         $id = test_input($_POST['id']);
         $feel = test_input($_POST['feeling']);
-
         $query = " 
             UPDATE hr_db 
             SET feeling = :feeling  
             WHERE 
-                id = :id
-        ";
-
+                id = :id ";
         $query_params = array( 
             ':id' => $id, 
-            ':feeling' => $feel 
-        ); 
-         
+            ':feeling' => $feel  );     
         try { 
             $stmt = $db->prepare($query); 
             $result = $stmt->execute($query_params); 
@@ -28,8 +21,6 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         catch(PDOException $ex) { 
             die("Failed to run query: " . $ex->getMessage()); 
         }
-
         echo "good";
 }//end post
-     
 ?>
