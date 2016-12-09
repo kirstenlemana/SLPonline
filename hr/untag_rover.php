7d5ce22cea6541cbc506fac44920ebd68c072360
kirstenlemana <kirstenlemana@gmail.com>
2016-12-09 10:50:01 +0800

    Update untag_rover.php - Adjust spacing and curly braces

--- hr/untag_rover.php
+++ hr/untag_rover.php
@@ -1,15 +1,11 @@
 <?php
 require "../zxcd9.php";
 //start post
-if(!empty($_POST)) 
-{ 
+if(!empty($_POST)) { 
 //filter input
 $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
-
         $rovid = test_input($_POST['rovid']);
         $hrid = test_input($_POST['hrid']);
-
         $query = "DELETE FROM hr_RVtags WHERE roverid = :rovid AND hrdbid = :hrdbid";
         $query_params = array( 
             ':rovid' => $rovid,
@@ -26,7 +22,5 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if ($stmt == true) {
             echo "good";    
         }
-        
 }//end post
-     
 ?>
